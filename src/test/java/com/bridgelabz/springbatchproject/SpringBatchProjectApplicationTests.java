42e7adcde298361b411ba11a9030f6dec2222ec7
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-09-13 13:44:15 +0530

    Added: Spring batch project

---
+++ src/test/java/com/bridgelabz/springbatchproject/SpringBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springbatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
